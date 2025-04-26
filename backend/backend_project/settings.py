bbee0ff6a2d1293498340b57fd787de13a5db0c4
itjosephk2 <josephkeane996@gmail.com>
2025-04-26 20:52:46 +0100

    Set up environment variables with python-decouple

--- backend/backend_project/settings.py
+++ backend/backend_project/settings.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+from decouple import config
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -8,10 +10,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q^)n63pnw@fu=wq*6j%55$h@f9l!i0nn2au0h9c9s-k!8$zpu%'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = []
 
